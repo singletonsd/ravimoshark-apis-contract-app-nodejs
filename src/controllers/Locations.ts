7ba70fc0be02316e25c19c94b757df0331828ff3
dev <dev@ravimoshark.com>
2019-10-18 16:12:10 +0200

    Added new spec.

---
+++ src/controllers/Locations.ts
@@ -0,0 +1,65 @@
+"use strict";
+
+import { LocationsService } from "../service";
+import { CustomNext, CustomRequest, CustomResponse } from "../utils/customsHandlers";
+import { ParametersComplete, ParametersIdDeleted, Utilities } from "../utils/utilities";
+import { ResponsePayload } from "../utils/writer";
+
+module.exports.addLocation = (req: CustomRequest, res: CustomResponse, next: CustomNext) => {
+  const body = Utilities.checkVariableNotNull(req.swagger.params.undefined.originalValue, res);
+  LocationsService.add(body)
+    .then((response: any) => {
+      ResponsePayload.response(res, response);
+    }).catch((response: any) => {
+      ResponsePayload.response400(res, response);
+    });
+};
+
+module.exports.deleteLocation = (req: CustomRequest, res: CustomResponse, next: CustomNext) => {
+  const id = Utilities.checkIdAndDelete(req.swagger.params, res);
+  if (!id) {
+    return;
+  }
+  LocationsService.delete(id)
+    .then((response: any) => {
+      ResponsePayload.response(res, response);
+    }).catch((response: any) => {
+      ResponsePayload.response400(res, response);
+    });
+};
+
+module.exports.editLocation = (req: CustomRequest, res: CustomResponse, next: CustomNext) => {
+  const body = Utilities.checkVariableNotNull(req.swagger.params.undefined.originalValue, res);
+  LocationsService.edit(body)
+    .then((response: any) => {
+      ResponsePayload.response(res, response);
+    }).catch((response: any) => {
+      ResponsePayload.response400(res, response);
+    });
+};
+
+module.exports.getLocationById = (req: CustomRequest, res: CustomResponse, next: CustomNext) => {
+  const params: ParametersIdDeleted = Utilities.checkIdAndDelete(req.swagger.params, res);
+  if (!params) {
+    return;
+  }
+  LocationsService.getById(params)
+    .then((response: any) => {
+      ResponsePayload.response(res, response);
+    }).catch((response: any) => {
+      ResponsePayload.response400(res, response);
+    });
+};
+
+module.exports.getLocations = (req: CustomRequest, res: CustomResponse, next: CustomNext) => {
+  const params: ParametersComplete = Utilities.checkAllParametersGet(req.swagger.params, res);
+  if (!params) {
+    return;
+  }
+  LocationsService.get(params)
+    .then((response: any) => {
+      ResponsePayload.response(res, response);
+    }).catch((response: any) => {
+      ResponsePayload.response400(res, response);
+    });
+};
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,6 @@
 export * from "./ClientsService";
 export * from "./ContractsService";
 export * from "./ImportedMachinesService";
+export * from "./LocationsService";
 export * from "./MachinesService";
 export * from "./PiecesService";
