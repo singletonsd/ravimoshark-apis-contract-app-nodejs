df759eb47ea243fa092c2cc9cfa4a4ac95232a5e
Patricio Rafael Perpetua <patricio.perpetua.arg@gmail.com>
2019-10-21 21:51:22 +0200

    Added exists client method.

--- src/service/ClientsService.ts
+++ src/service/ClientsService.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 import { getConnection } from "typeorm";
-import { Clients as ClientsDB } from "../databases/entities";
+import { Clients } from "../databases/entities";
 import { DatabaseUtilities } from "../databases/utils/DatabaseUtils";
 import { LoggerUtility } from "../utils/LoggerUtility";
 import { ParametersComplete, Utilities } from "../utils/utilities";
@@ -9,6 +9,28 @@ import { VALID_RESPONSES } from "../utils/ValidResponses";
 
 const SERVICE_NAME = "ClientsService";
 export class ClientsService {
+
+  /**
+   *  Check if a location exits.
+   * @param id
+   */
+  public static exists(refClient: string): Promise<boolean> {
+    const FUNCTION_NAME = "exists";
+    const logHeader = `${SERVICE_NAME}: ${FUNCTION_NAME} -`;
+    return new Promise<boolean>(async (resolve, reject) => {
+        const previous = await getConnection().manager.findOne(Clients, {
+            select: [ "refClient" ],
+            where: { refClient }
+        });
+        if (!previous) {
+            LoggerUtility.warn(`${logHeader} ${VALID_RESPONSES.ERROR.NOT_EXIST.CONTRACT} ${refClient}`);
+            resolve(false);
+            return;
+        }
+        resolve(true);
+        return;
+    });
+  }
 
   /**
    * Get all clients.
@@ -29,14 +51,14 @@ export class ClientsService {
     const logHeader = `${SERVICE_NAME}: ${FUNCTION_NAME} -`;
     return new Promise(async (resolve, reject) => {
       LoggerUtility.info(`${logHeader}`);
-      const object = DatabaseUtilities.getFindObject(params, ClientsDB);
+      const object = DatabaseUtilities.getFindObject(params, Clients);
       if (!object) {
           LoggerUtility.warn(`${logHeader} order param malformed`, params.orderBy);
           reject(VALID_RESPONSES.ERROR.PARAMS.MALFORMED.ORDERBY);
           return;
       }
       LoggerUtility.info(`${logHeader} with`, object);
-      const [items, total] = await getConnection().manager.findAndCount(ClientsDB, object);
+      const [items, total] = await getConnection().manager.findAndCount(Clients, object);
       if (!items || !items.length) {
           LoggerUtility.warn(`${logHeader} empty result`);
           resolve();
